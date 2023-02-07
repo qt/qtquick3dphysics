220a040c32f9123eda6a3e79a7de9235f4309fe2
Jonas Karlsson <jonas.karlsson@qt.io>
2023-02-07 05:15:14 +0100

    Properly name class
    Pick-to: 6.5
    Change-Id: I24f02a08e181a5b5394400ac905c3f8e867a10e4
    Reviewed-by: Hatem ElKharashy <hatem.elkharashy@qt.io>
    Reviewed-by: Paul Olav Tvete <paul.tvete@qt.io>

--- tests/auto/multiscene/tst_multiscene.cpp
+++ tests/auto/multiscene/tst_multiscene.cpp
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
 
 #include <QtQuickTest/quicktest.h>
-class tst_physicsscene: public QObject
+class tst_multiscene: public QObject
 {
     Q_OBJECT
 private slots:
@@ -12,10 +12,10 @@ int main(int argc, char **argv)
 {
     if (!qEnvironmentVariableIsEmpty("QEMU_LD_PREFIX")) {
         qWarning("This test is unstable on QEMU, so it will be skipped.");
-        tst_physicsscene skip;
+        tst_multiscene skip;
         return QTest::qExec(&skip, argc, argv);
     }
     QTEST_SET_MAIN_SOURCE_PATH
-    return quick_test_main(argc, argv, "tst_physicsscene", QUICK_TEST_SOURCE_DIR);
+    return quick_test_main(argc, argv, "tst_multiscene", QUICK_TEST_SOURCE_DIR);
 }
 #include "tst_multiscene.moc"
