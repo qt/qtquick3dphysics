1eb24a47b3e87c494ec6d19e1c340c900991bf9a
Jonas Karlsson <jonas.karlsson@qt.io>
2023-08-03 13:26:37 +0200

    Refactor: move QPhysXActorBody into own file
    Change-Id: I48fee67fa9a64cab2c55a891fa357686e83df09b
    Reviewed-by: Paul Olav Tvete <paul.tvete@qt.io>
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    (cherry picked from commit 22244d4db16c1bb832099de3ddd434272b89a9ea)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/quick3dphysics/qphysicsworld.cpp
+++ src/quick3dphysics/qphysicsworld.cpp
@@ -4,6 +4,7 @@
 #include "qphysicsworld_p.h"
 
 #include "physxnode/qabstractphysxnode_p.h"
+#include "physxnode/qphysxactorbody_p.h"
 #include "physxnode/qphysxworld_p.h"
 #include "qabstractphysicsnode_p.h"
 #include "qcapsuleshape_p.h"
@@ -12,11 +13,9 @@
 #include "qtriggerbody_p.h"
 #include "qdynamicrigidbody_p.h"
 #include "qstaticrigidbody_p.h"
-#include "qplaneshape_p.h"
 #include "qphysicscommands_p.h"
 #include "qstaticphysxobjects_p.h"
 #include "qcharactercontroller_p.h"
-#include "qheightfieldshape_p.h"
 
 #include "PxPhysicsAPI.h"
 #include "cooking/PxCooking.h"
@@ -164,27 +163,6 @@ static physx::PxTransform getPhysXWorldTransform(const QMatrix4x4 transform)
                               QPhysicsUtils::toPhysXType(rotation));
 }
 
-static physx::PxTransform getPhysXLocalTransform(const QQuick3DNode *node)
-{
-    // Modify transforms to make the PhysX shapes match the QtQuick3D conventions
-    if (qobject_cast<const QPlaneShape *>(node) != nullptr) {
-        // Rotate the plane to make it match the built-in rectangle
-        const QQuaternion rotation = kMinus90YawRotation * node->rotation();
-        return physx::PxTransform(QPhysicsUtils::toPhysXType(node->position()),
-                                  QPhysicsUtils::toPhysXType(rotation));
-    } else if (auto *hf = qobject_cast<const QHeightFieldShape *>(node)) {
-        // Shift the height field so it's centered at the origin
-        return physx::PxTransform(QPhysicsUtils::toPhysXType(node->position() + hf->hfOffset()),
-                                  QPhysicsUtils::toPhysXType(node->rotation()));
-    }
-
-    const QQuaternion &rotation = node->rotation();
-    const QVector3D &localPosition = node->position();
-    const QVector3D &scale = node->sceneScale();
-    return physx::PxTransform(QPhysicsUtils::toPhysXType(localPosition * scale),
-                              QPhysicsUtils::toPhysXType(rotation));
-}
-
 class ControllerCallback : public physx::PxUserControllerHitReport
 {
 public:
@@ -232,31 +210,6 @@ public:
 private:
     physx::PxCapsuleController *controller = nullptr;
     ControllerCallback *reportCallback = nullptr;
-};
-
-class QPhysXActorBody : public QAbstractPhysXNode
-{
-public:
-    QPhysXActorBody(QAbstractPhysicsNode *frontEnd) : QAbstractPhysXNode(frontEnd) { }
-    void cleanup(QPhysXWorld *physX) override
-    {
-        if (actor) {
-            physX->scene->removeActor(*actor);
-            PHYSX_RELEASE(actor);
-        }
-        QAbstractPhysXNode::cleanup(physX);
-    }
-    void init(QPhysicsWorld *world, QPhysXWorld *physX) override;
-    void sync(float deltaTime, QHash<QQuick3DNode *, QMatrix4x4> &transformCache) override;
-    void markDirtyShapes() override;
-    void rebuildDirtyShapes(QPhysicsWorld *world, QPhysXWorld *physX) override;
-    virtual void createActor(QPhysXWorld *physX);
-
-    bool debugGeometryCapability() override { return true; }
-    physx::PxTransform getGlobalPose() override { return actor->getGlobalPose(); }
-    void buildShapes(QPhysXWorld *physX);
-
-    physx::PxRigidActor *actor = nullptr;
 };
 
 class QPhysXRigidBody : public QPhysXActorBody
@@ -316,10 +269,6 @@ public:
 };
 
 
-
-
-
-
 void QPhysXCharacterController::createMaterial(QPhysXWorld *physX)
 {
     createMaterialFromQtMaterial(physX, static_cast<QCharacterController *>(frontendNode)->physicsMaterial());
@@ -330,32 +279,12 @@ void QPhysXRigidBody::createMaterial(QPhysXWorld *physX)
     createMaterialFromQtMaterial(physX, static_cast<QAbstractPhysicsBody *>(frontendNode)->physicsMaterial());
 }
 
-void QPhysXActorBody::createActor(QPhysXWorld * /*physX*/)
-{
-    auto& s_physx = StaticPhysXObjects::getReference();
-    const physx::PxTransform trf = QPhysicsUtils::toPhysXTransform(frontendNode->scenePosition(),
-                                                                   frontendNode->sceneRotation());
-    actor = s_physx.physics->createRigidDynamic(trf);
-}
-
 void QPhysXStaticBody::createActor(QPhysXWorld * /*physX*/)
 {
     auto& s_physx = StaticPhysXObjects::getReference();
     const physx::PxTransform trf = QPhysicsUtils::toPhysXTransform(frontendNode->scenePosition(),
                                                                    frontendNode->sceneRotation());
     actor = s_physx.physics->createRigidStatic(trf);
-}
-
-void QPhysXActorBody::init(QPhysicsWorld *, QPhysXWorld *physX)
-{
-    Q_ASSERT(!actor);
-
-    createMaterial(physX);
-    createActor(physX);
-
-    actor->userData = reinterpret_cast<void *>(frontendNode);
-    physX->scene->addActor(*actor);
-    setShapesDirty(true);
 }
 
 void QPhysXCharacterController::cleanup(QPhysXWorld *physX)
@@ -432,77 +361,6 @@ DebugDrawBodyType QPhysXDynamicBody::getDebugDrawBodyType()
     auto dynamicActor = static_cast<physx::PxRigidDynamic *>(actor);
     return dynamicActor->isSleeping() ? DebugDrawBodyType::DynamicSleeping
                                       : DebugDrawBodyType::DynamicAwake;
-}
-
-void QPhysXActorBody::markDirtyShapes()
-{
-    if (!frontendNode || !actor)
-        return;
-
-    // Go through the shapes and look for a change in pose (rotation, position)
-    // TODO: it is likely cheaper to connect a signal for changes on the position and rotation
-    // property and mark the node dirty then.
-    if (!shapesDirty()) {
-        const auto &collisionShapes = frontendNode->getCollisionShapesList();
-        const auto &physXShapes = shapes;
-
-        const int len = collisionShapes.size();
-        if (physXShapes.size() != len) {
-            // This should not really happen but check it anyway
-            setShapesDirty(true);
-        } else {
-
-            for (int i = 0; i < len; i++) {
-                auto poseNew = getPhysXLocalTransform(collisionShapes[i]);
-                auto poseOld = physXShapes[i]->getLocalPose();
-
-                if (!fuzzyEquals(poseNew, poseOld)) {
-                    setShapesDirty(true);
-                    break;
-                }
-            }
-        }
-    }
-}
-
-void QPhysXActorBody::buildShapes(QPhysXWorld * /*physX*/)
-{
-    auto body = actor;
-    for (auto *shape : shapes) {
-        body->detachShape(*shape);
-        PHYSX_RELEASE(shape);
-    }
-
-    // TODO: Only remove changed shapes?
-    shapes.clear();
-
-    for (const auto &collisionShape : frontendNode->getCollisionShapesList()) {
-        // TODO: shapes can be shared between multiple actors.
-        // Do we need to create new ones for every body?
-        auto *geom = collisionShape->getPhysXGeometry();
-        if (!geom || !material)
-            continue;
-
-        auto& s_physx = StaticPhysXObjects::getReference();
-        auto physXShape = s_physx.physics->createShape(*geom, *material);
-
-        if (useTriggerFlag()) {
-            physXShape->setFlag(physx::PxShapeFlag::eSIMULATION_SHAPE, false);
-            physXShape->setFlag(physx::PxShapeFlag::eTRIGGER_SHAPE, true);
-        }
-
-        shapes.push_back(physXShape);
-        physXShape->setLocalPose(getPhysXLocalTransform(collisionShape));
-        body->attachShape(*physXShape);
-    }
-}
-
-void QPhysXActorBody::rebuildDirtyShapes(QPhysicsWorld *, QPhysXWorld *physX)
-{
-    if (!shapesDirty())
-        return;
-    buildShapes(physX);
-    setShapesDirty(false);
 }
 
 void QPhysXDynamicBody::rebuildDirtyShapes(QPhysicsWorld *world, QPhysXWorld *physX)
@@ -628,28 +486,6 @@ static physx::PxRigidDynamicLockFlags getLockFlags(QDynamicRigidBody *body)
                        ? physx::PxRigidDynamicLockFlag::eLOCK_LINEAR_Z
                        : 0);
     return static_cast<physx::PxRigidDynamicLockFlags>(flags);
-}
-
-static void updatePhysXMaterial(const QPhysicsMaterial *qtMaterial,
-                                physx::PxMaterial *physXMaterial)
-{
-    const float staticFriction = qtMaterial->staticFriction();
-    const float dynamicFriction = qtMaterial->dynamicFriction();
-    const float restitution = qtMaterial->restitution();
-    if (physXMaterial->getStaticFriction() != staticFriction)
-        physXMaterial->setStaticFriction(staticFriction);
-    if (physXMaterial->getDynamicFriction() != dynamicFriction)
-        physXMaterial->setDynamicFriction(dynamicFriction);
-    if (physXMaterial->getRestitution() != restitution)
-        physXMaterial->setRestitution(restitution);
-}
-
-void QPhysXActorBody::sync(float /*deltaTime*/, QHash<QQuick3DNode *, QMatrix4x4> & /*transformCache*/)
-{
-    auto *body = static_cast<QAbstractPhysicsBody *>(frontendNode);
-    if (QPhysicsMaterial *qtMaterial = body->physicsMaterial()) {
-        updatePhysXMaterial(qtMaterial, material);
-    }
 }
 
 DebugDrawBodyType QPhysXTriggerBody::getDebugDrawBodyType()
