f6303e71a9c1f8a2dde708ce763390d59b674528
Jonas Karlsson <jonas.karlsson@qt.io>
2023-08-03 12:40:33 +0200

    Refactor: create Q(Static|Dynamic)RigidBody files
    Pick-to: 6.5 6.6
    Change-Id: I300083addea0b65ee03836605d9c45e817e67d2b
    Reviewed-by: Paul Olav Tvete <paul.tvete@qt.io>

--- src/quick3dphysics/qdynamicrigidbody.cpp
+++ src/quick3dphysics/qdynamicrigidbody.cpp
@@ -1,7 +1,7 @@
 // Copyright (C) 2021 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
 
-#include "qrigidbody_p.h"
+#include "qdynamicrigidbody_p.h"
 #include "qphysicscommands_p.h"
 #include "qphysicsworld_p.h"
 
@@ -647,21 +647,5 @@ QVector3D QDynamicRigidBody::kinematicPosition() const
 {
     return m_kinematicPosition;
 }
-
-/*!
-    \qmltype StaticRigidBody
-    \inqmlmodule QtQuick3D.Physics
-    \inherits PhysicsBody
-    \since 6.4
-    \brief A physical body that does not move.
-
-    The StaticRigidBody type defines an immovable and static rigid body. Any collision shape is allowed for this body.
-
-    \note Do not move a StaticRigidBody. It is technically possible to do so, but it
-    will incur a performance penalty, and colliding dynamic objects may not react correctly.
-    Use a DynamicRigidBody with \l {DynamicRigidBody::isKinematic}{isKinematic} set to \c true instead.
-*/
-
-QStaticRigidBody::QStaticRigidBody() = default;
 
 QT_END_NAMESPACE
