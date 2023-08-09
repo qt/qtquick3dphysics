cc67387b15f16e40e68e19f4a019357dae7f237c
Jonas Karlsson <jonas.karlsson@qt.io>
2023-08-09 03:44:16 +0200

    Make StaticPhysXObjects a QGlobalStatic
    Change-Id: Ic5079364a22ba73cc3af389d9fd8d9b8dd046ac0
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Paul Olav Tvete <paul.tvete@qt.io>
    (cherry picked from commit d957a7aad367e3fa838547169792b025fa660720)
    Reviewed-by: Jonas Karlsson <jonas.karlsson@qt.io>

--- src/quick3dphysics/qstaticphysxobjects.cpp
+++ src/quick3dphysics/qstaticphysxobjects.cpp
@@ -2,14 +2,16 @@
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
 
 #include "qstaticphysxobjects_p.h"
+#include <QGlobalStatic>
+#include <new>
 
 QT_BEGIN_NAMESPACE
 
-StaticPhysXObjects s_physx = StaticPhysXObjects();
+Q_GLOBAL_STATIC(StaticPhysXObjects, s_physx);
 
 StaticPhysXObjects &StaticPhysXObjects::getReference()
 {
-    return s_physx;
+    return *s_physx;
 }
 
 QT_END_NAMESPACE
