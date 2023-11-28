3c5c19bc4b5c12d31e0fd966194b3cc0ccd2f72c
Jonas Karlsson <jonas.karlsson@qt.io>
2023-11-28 14:27:00 +0100

    Move almost identical code into QMeshShape
    QTriangleMeshShape and QConvexMeshShape are basically identical so move
    the common code into a new class QMeshShape. Using an enum
    ShapeType to differentiate the mesh types.
    
    Task-number: QTBUG-109818
    Change-Id: Ia2e72eb84d14bcf1a38fa0d2df046805d175a5ad
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Andy Nichols <andy.nichols@qt.io>
    (cherry picked from commit f43d8bf1aed47da1ae13d73d59e439ce6ef53348)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 94b65b21543d47591550e949bf44e529e70305e8)
    Reviewed-by: Jonas Karlsson <jonas.karlsson@qt.io>

--- src/quick3dphysics/qtrianglemeshshape_p.h
+++ src/quick3dphysics/qtrianglemeshshape_p.h
@@ -15,47 +15,15 @@
 // We mean it.
 //
 
-#include <QtQuick3DPhysics/qtquick3dphysicsglobal.h>
-#include <QtQuick3DPhysics/private/qabstractcollisionshape_p.h>
-#include <QtCore/QObject>
-#include <QtGui/QVector3D>
-#include <QtQml/QQmlEngine>
-
-namespace physx {
-class PxBoxGeometry;
-class PxTriangleMesh;
-class PxTriangleMeshGeometry;
-}
+#include "qmeshshape_p.h"
 
 QT_BEGIN_NAMESPACE
-class QQuick3DPhysicsMesh;
 
-class Q_QUICK3DPHYSICS_EXPORT QTriangleMeshShape : public QAbstractCollisionShape
+class Q_QUICK3DPHYSICS_EXPORT QTriangleMeshShape : public QMeshShape
 {
     Q_OBJECT
-    Q_PROPERTY(QUrl source READ source WRITE setSource NOTIFY sourceChanged REVISION(6, 5))
-
     QML_NAMED_ELEMENT(TriangleMeshShape)
-public:
-    QTriangleMeshShape();
-    ~QTriangleMeshShape();
-
-    physx::PxGeometry *getPhysXGeometry() override;
-    Q_REVISION(6, 5) const QUrl &source() const;
-    Q_REVISION(6, 5) void setSource(const QUrl &newSource);
-    bool isStaticShape() const override { return true; }
-
-signals:
-    Q_REVISION(6, 5) void sourceChanged();
-
-private:
-    void updatePhysXGeometry();
-
-    bool m_dirtyPhysx = false;
-    QUrl m_meshSource;
-    QQuick3DPhysicsMesh *m_mesh = nullptr;
-
-    physx::PxTriangleMeshGeometry *m_meshGeometry = nullptr;
+    virtual QMeshShape::MeshType shapeType() const override;
 };
 
 QT_END_NAMESPACE
