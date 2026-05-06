ac0deafba679252edeb3c5dc12041bf29f0eb029
Jonas Karlsson <jonas.karlsson@qt.io>
2026-05-06 11:34:24 +0200

    Clear lists on cleanup
    Should avoid potential dangling pointers.
    
    Pick-to: 6.11 6.8
    Change-Id: Ic5668ebb158fa67773fc90855bab6e91a83266ba
    Reviewed-by: Christian Strømme <christian.stromme@qt.io>

--- src/quick3dphysics/physxnode/qabstractphysxnode.cpp
+++ src/quick3dphysics/physxnode/qabstractphysxnode.cpp
@@ -85,6 +85,7 @@ void QAbstractPhysXNode::cleanup(QPhysXWorld *)
         PHYSX_RELEASE(shape);
     if (material != sDefaultMaterial)
         PHYSX_RELEASE(material);
+    shapes.clear();
 }
 
 bool QAbstractPhysXNode::debugGeometryCapability()
